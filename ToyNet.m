d19402f650b5e0648866706733939f32a76018cd
lost.dev.found <lost.dev.found@gmail.com>
2018-05-16 22:25:38 -0700

    fixes

--- ToyNet.m
+++ ToyNet.m
@@ -1,14 +1,13 @@
 classdef ToyNet
     properties
         numHiddenLayers;
-        numActivationLayers;
         inputLayerSize;
         outputLayerSize;
         hiddenLayersSize;
-        listOfWeightMatr;
-        listOfBiasVecs;
-        Y;      % Matrix Y. Each column of the matrix Y represents activation layer, rows represent the number of neurons in the layer
-        D;      % Matrix delta. Each column of the matirx D represent the derivative of of the CostFunction w.r.t. z vector (z = Wa+b)
+        arrayWeights;
+        arrayBiases;
+        Y;      % An array Y stores activation layaer vectors
+        D;      % An arrat that stores Delta vectors. Delta represent the derivative of of the CostFunction w.r.t. z vector (z = Wa+b)
     end
 
     methods
@@ -18,33 +17,35 @@ classdef ToyNet
             obj.inputLayerSize = i_numHiddenLayers;
             obj.outputLayerSize = i_outputLayerSize;
             obj.hiddenLayersSize = i_hiddenLayersSize;
-            obj.numActivationLayers = i_numHiddenLayers + 1;
+            obj.totalNumLayers = i_numHiddenLayers + 2;
             rng(5000); % seed generator
 
-            % Build a matrix s.t. columns
-            obj.Y = zeros(obj.hiddenLayersSize, obj.numActivationLayers);    % init matrix Y as matrix with all enries 0
-            obj.D = zeros(obj.hiddenLayersSize, obj.numActivationLayers);    % init matrix Y as matrix with all enries 0
+            % Init arrays
+            obj.D{1} = 0;
+            obj.Y{1} = 0;
+            obj.Y{2} = zeros(obj.hiddenLayersSize, 1);    % init array Y as matrix with all enries 0
+            obj.D{2} = zeros(obj.hiddenLayersSize, 1);    % init array D as matrix with all enries 0
 
             % Build W2, b2 for connections from input layer to first hidden
             W2 = 0.5*rand(obj.hiddenLayersSize, obj.inputLayerSize);
             b2 = 0.5*rand(obj.hiddenLayersSize,1);
 
-            obj.listOfWeightMatr = W2;
-            obj.listOfBiasVecs = b2;
+            obj.arrayWeights{2} = W2;
+            obj.arrayBiases{2} = b2;
 
             % Build intermediate W and b
-            for i = 1:obj.numHiddenLayers - 1   % do not build W and b from last hidden layer to output layer
+            for i = 3:obj.totalNumLayers - 1   % do not build W and b from last hidden layer to output layer
                 W = 0.5*rand(obj.hiddenLayersSize, obj.hiddenLayersSize);
                 b = 0.5*rand(obj.hiddenLayersSize,1);
-                obj.listOfWeightMatr(:,:,i+1) = W;
-                obj.listOfBiasVecs(:,i+1) = b;
+                obj.arrayWeights{i} = W;
+                obj.arrayBiases{i} = b;
             end
 
             % Build W and b from last hidden layer to output layer
             WN = 0.5*rand(obj.outputLayerSize, obj.hiddenLayersSize);
             bN = 0.5*rand(obj.outputLayerSize, 1);
-            obj.listOfWeightMatr(:,:, obj.numHiddenLayers) = WN;
-            obj.listOfBiasVecs(:, obj.numHiddenLayers) = bN;
+            obj.arrayWeights{i + 1} = WN;
+            obj.arrayBiases{i + 1} = bN;
 
         end
 
@@ -52,11 +53,11 @@ classdef ToyNet
         % Forward propagation
         function result = forwardProp(i_vector)
             % activate first hidden layer
-            obj.Y(:,1) = activate(i_vector, obj.listOfWeightMatr(1), obj.listOfBiasVecs(1));
+            obj.Y{2} = activate(i_vector, obj.arrayWeights{2}, obj.arrayBiases{2});
 
             % activate other consequent layers plus output layer
-            for i = 2:obj.numActivationLayers
-                obj.Y(:,i) = activate(a, obj.listOfWeightMatr(:,:,i), obj.listOfBiasvecs);
+            for i = 3:obj.totalNumLayers
+                obj.Y{i} = activate(a, obj.arrayWeights{i}, obj.arrayBiases{i});
             end
 
             result = a;
@@ -65,20 +66,20 @@ classdef ToyNet
 
 
         % Back propagation
-        function backresult = backprop(i_vector)
+        function backresult = backprop(label_vector)
             % Backward pass
-            Yn = obj.numActivationLayers;
-            obj.D(:,Yn) = obj.Y(:,Yn).*(1-obj.Y(:,Yn)).*(obj.Y(:,Yn)-i_vector);
+            YN = obj.totalNumLayers;
+            obj.D{YN} = obj.Y{YN} .* (1 - obj.Y{YN}) .* (obj.Y{YN} - label_vector);
 
-            for i = Yn-1:-1:1
-                obj.D(:,i) = obj.Y(:,i).*(1-obj.Y(:,i)).*(obj.listOfWeightMatr(i+1)' * obj.D(i+1));
+            for i = YN-1:-1:2
+                obj.D{i} = obj.Y{i} .* (1 - obj.Y{i}) .* (obj.arrayWeights{i+1}' * obj.D{i+1});
             end
 
             % Gradient step
-            obj.listOfWeightMatr(:,:,1) = obj.listOfWeightMatr(:,:,1) - eta * obj.D(1) * i_vector';
+            obj.arrayWeights{2} = obj.arrayWeights{2} - eta * obj.D{2} * i_vector';
 
-            for i = 2:Yn
-                obj.listOfWeightMatr(:,:,i) = obj.listOfWeightMatr(:,:,i) - eta * obj.D(i) * obj.Y(i-1)
+            for i = 3:YN
+                obj.arrayWeights{i} = obj.arrayWeights{i} - eta * obj.D{i} * obj.Y{i - 1}
             end
 
             % delta4 = a4.*(1-a4).*(a4-y(:,k));    % derivSigmra*derivCostWRTa4
